349866bfc768ef1015462248203cc3268a758403
saurabh_malode <saurabh_malode@yahoo.co.in>
2015-02-07 01:35:06 +0530

    added playlist class

---
+++ MusicLibraryBuilder/src/com/skm/mlb/playlist/Playlist.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.skm.mlb.playlist;
+
+import java.util.ArrayList;
+
+/**
+ * @author intel
+ *
+ */
+public class Playlist {
+
+	private String name;
+	private ArrayList<String> songList;
+	
+	
+	
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+
+	/**
+	 * @return the songList
+	 */
+	public ArrayList<String> getSongList() {
+		return songList;
+	}
+
+
+
+
+
+	/**
+	 * @param songList the songList to set
+	 */
+	public void setSongList(ArrayList<String> songList) {
+		this.songList = songList;
+	}
+
+
+
+
+
+	/**
+	 * 
+	 */
+	public Playlist() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
